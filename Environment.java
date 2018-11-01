0f46cec20dd3f5324bfaec1665a4691bcdd7107e
adamwrice <44509408+adamwrice@users.noreply.github.com>
2018-11-01 15:47:04 -0400

    Create Environment.java

---
+++ Environment.java
@@ -0,0 +1,16 @@
+public class Environment {
+
+public String[] events;
+public int daysPast;
+public double progress;
+
+public void createEvent(){
+}
+
+public void trackDaysPast(){
+}
+
+public void displayProgess(){
+}
+
+}

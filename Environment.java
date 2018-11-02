c1c46c6200f3e56eca09b19ca9046fb5c435ba0c
adamwrice <44509408+adamwrice@users.noreply.github.com>
2018-11-02 16:17:17 -0400

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -1,13 +1,20 @@
+import java.lang.*;
+
 public class Environment {
 
 public String[] events;
-public int daysPast;
-public double progress;
+public int daysPast = 0;
+public int timeFrame = 40*(int)(35*Math.random()+1);
+public int budget;
+public double progress = 0.0;
 
 public void createEvent(){
 }
 
 public void trackDaysPast(){
+  if (daysPast > timeFrame){
+    //lose game
+  }
 }
 
 public void displayProgess(){

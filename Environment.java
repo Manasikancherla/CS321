a7ab0257f0859d65f6d2a41933f856cdd0de2745
adamwrice <44509408+adamwrice@users.noreply.github.com>
2018-11-08 15:34:25 -0500

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -3,8 +3,9 @@ import java.lang.*;
 public class Environment {
 
 public String[] events;
+public NPC[] current_employ;
 public int daysPast = 0;
-public int timeFrame = 40*(int)(35*Math.random()+1);
+public int timeFrame;
 public int budget;
 public double progress = 0.0;
 
@@ -18,6 +19,27 @@ public void trackDaysPast(){
 }
 
 public void displayProgess(){
+  System.out.println("Total Progress: " + progress);
+}
+  
+public void setCurrentEmploy(NPC[] employees){
+  current_employ = employees;
+}
+  
+public void nextDay(){
+  daysPast++;
+}
+  
+public void setTimeFrame(int newTimeFrame){
+  timeFrame = newTimeFrame; //40*(int)(35*Math.random()+1);
+}
+  
+public void setBudget(int newBudget){
+  budget = newBudget;
+}
+
+public void setProgress(double newProgress){
+  progress += (newProgress/100);
 }
 
 }

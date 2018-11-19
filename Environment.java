9c97823d43a92b8cf9ade98193c0d0ddfed8df59
adamwrice <44509408+adamwrice@users.noreply.github.com>
2018-11-19 14:46:54 -0500

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -10,11 +10,33 @@ public int budget;
 public double progress = 0.0;
 
 public void createEvent(){
+  Scanner s = new Scanner(System.in);
+  System.out.println("DAY " + daysPast);
+  System.out.println("It's a new day at the office! You sit down at your desk and check your email. Here's what' going on around the office today:");
+  System.out.println("");
+  //event call
+  System.out.println("");
+  displayProgress();
+  //display player commmands (e.g. Hire, Fire, Next Day)
+  System.out.println("");
+  System.out.println("H = Hire       F = Fire      T = Advance Day");
+  if(s.next() == "h" || "H"){
+    //Hire
+  }
+  else if(s.next() == "f" || "F"){
+    //Fire
+  }
+  else if(s.next() == "t" || "T"){
+    nextDay();
+  }
+  else {
+  }
 }
 
 public void trackDaysPast(){
   if (daysPast > timeFrame){
     //lose game
+    System.out.println("You get a call from the manager. He's very upset! Seems like you've gone over time. He says he's assigning somone new that will manage things properly. Better pack up your things! [GAME OVER]");
   }
 }
 
@@ -28,6 +50,25 @@ public void setCurrentEmploy(NPC[] employees){
   
 public void nextDay(){
   daysPast++;
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+  createEvent();
 }
   
 public void setTimeFrame(int newTimeFrame){

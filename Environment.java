4a5862587edda1d78cc28dbfec098b3e671243a3
adamwrice <44509408+adamwrice@users.noreply.github.com>
2018-11-19 19:04:18 -0500

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -2,50 +2,70 @@ import java.lang.*;
 
 public class Environment {
 
-public String[] events;
-public NPC[] current_employ;
-public int daysPast = 0;
-public int timeFrame;
-public int budget;
-public double progress = 0.0;
-
-public void createEvent(){
+public Arraylist<NPC> atOffice = new Arraylist<NPC>();
+public Arraylist<NPC> outToday = new Arraylist<NPC>();
+private Arraylist<NPC> team;
+  
+public hire(NPC n){
+  team.add(n);
+  atOffice.add(n);
+}
+  
+public fire(NPC n){
+  team.remove(n);
+  if (atOffice.contains(n)){
+    atOffice.remove(n);
+  }
+  else if (outToday.contains(n)){
+    outToday.remove(n);
+  }
+}
+  
+public leave(NPC n){
+  outToday.add(atOffice.remove(n));
+}
+  
+public returnToOffice(NPC n){
+  atOffice.add(outToday.remove(n));
+}
+  
+/*public void writeEvent(){
   Scanner s = new Scanner(System.in);
   System.out.println("DAY " + daysPast);
+  System.out.println("");
   System.out.println("It's a new day at the office! You sit down at your desk and check your email. Here's what' going on around the office today:");
   System.out.println("");
-  //event call
+  AIEnvironment.createEvent(); //event call
   System.out.println("");
   displayProgress();
-  //display player commmands (e.g. Hire, Fire, Next Day)
   System.out.println("");
-  System.out.println("H = Hire       F = Fire      T = Advance Day");
+  System.out.println("H = Hire       F = Fire      T = Advance Day"); //display player commmands (e.g. Hire, Fire, Next Day)
   if(s.next() == "h" || "H"){
-    //Hire
+     AIEnvironment.hireEmployee(); //Hire
   }
   else if(s.next() == "f" || "F"){
-    //Fire
+    AIEnvironment.fireEmployee();//Fire
   }
   else if(s.next() == "t" || "T"){
     nextDay();
   }
   else {
   }
-}
+}*/
 
-public void trackDaysPast(){
-  if (daysPast > timeFrame){
+public boolean trackDaysPast(){
+  if (AIEnvironment.getDaysPast() > AIEnvironment.getDays()){
     //lose game
     System.out.println("You get a call from the manager. He's very upset! Seems like you've gone over time. He says he's assigning somone new that will manage things properly. Better pack up your things! [GAME OVER]");
+    return false;
+  }
+  else {
+   return true;
   }
 }
 
 public void displayProgess(){
   System.out.println("Total Progress: " + progress);
-}
-  
-public void setCurrentEmploy(NPC[] employees){
-  current_employ = employees;
 }
   
 public void nextDay(){
@@ -68,15 +88,14 @@ public void nextDay(){
   System.out.println("");
   System.out.println("");
   System.out.println("");
-  createEvent();
+  writeEvent();
 }
   
-public void setTimeFrame(int newTimeFrame){
-  timeFrame = newTimeFrame; //40*(int)(35*Math.random()+1);
-}
-  
-public void setBudget(int newBudget){
-  budget = newBudget;
+public String displayTeam(){
+  for (NPC n: team){
+   System.out.print(n.getName() + " ");
+   System.out.println("");
+  }
 }
 
 public void setProgress(double newProgress){

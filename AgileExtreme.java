196a1fdce783adf57a16a5d23fd803cc678b5653
Reversedsoul <44274544+Reversedsoul@users.noreply.github.com>
2018-11-25 22:48:03 -0500

    Adjustments to make it work with AIEnvironment

--- AgileExtreme.java
+++ AgileExtreme.java
@@ -1,31 +1,29 @@
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class AgileExtreme {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws FileNotFoundException{
     		AIEnvironment game = new AIEnvironment();
     		Scanner scanner = new Scanner(System.in);
     		String input;
     		String command = "";
     		boolean boost = false;
-    		int days = 0;
     		startMessage();
     		System.out.println("\nUse the command \"Help\" to get all the commands.");
-    		while(true) {
+    		while(game.running()) {
     			System.out.println("\nWhat would you like to do today?");
     			input = scanner.next();
     			switch(input) {
-    				case "quit":		
-    					System.exit(0);
+    				case "quit":
     					break;
     								
     				case "hire":		
     					while(true) {
     						System.out.println("\nHere is a list of possible candidates!");
-    						//game.RecruitmentList();
+    						game.displayHire();
     						System.out.println("Which npc would you like to hire? (Name of NPC)");
     						command = scanner.next();
-    						if(command.equals("steve")/*game.hire(command)*/) {
+    						if(game.hire(command)) {
     							System.out.println("\nCongratulations you have hired" + command);
     							break;
     						}else if(command.equals("quit")){
@@ -41,10 +39,10 @@ public class AgileExtreme {
     				case "fire":		
     					while(true) {
     						System.out.println("\nHere is a list of the current members of the development team");
-    						//Game.showTeam();
+    						game.displayTeam();
     						System.out.println("Which npc would you like to fire? (Name of NPC)");
     						command = scanner.next();
-    						if(command.equals("john")/*game.fire(command)*/) {
+    						if(game.fire(command)) {
     							System.out.println(command + "\nHas been removed from the team.");
     							break;
     						}else if(command.equals("quit")){
@@ -58,7 +56,7 @@ public class AgileExtreme {
     					break;
     				case "stats":	
     					System.out.println("\nHere are the stats of the members of the development team!");
-    					//Game.displayTeam();
+    					game.displayStats();
     					break;
     				case "rest":			
     					while(true) {
@@ -89,37 +87,28 @@ public class AgileExtreme {
     							//game.displayBoost
     							System.out.println("Please pick one.");		
     							switch(scanner.nextInt()) {
-    								case 1:
-    									/*
-    									if(game.boost(1)){
-    										System.out.println("You team feels slightly more motivated.");
+    								case 1:  									
+    									if(game.useBoost(1)){
     										boost = true;
     									}else{
     										System.out.println("You do not have enough money to do this.");
     									}
-    									*/
     									boost = true;
     									break;
     								case 2:
-    									/*
-    									if(game.boost(2)){
-    										System.out.println("You team feels slightly more motivated.");
+    									if(game.useBoost(2)){
     										boost = true;
     									}else{
     										System.out.println("You do not have enough money to do this.");
     									}
-    									*/
     									boost = true;
     									break;
     								case 3:
-    									/*
-    									if(game.boost(3)){
-    										System.out.println("You team feels slightly more motivated.");
+    									if(game.useBoost(3)){
     										boost = true;
     									}else{
     										System.out.println("You do not have enough money to do this.");
     									}
-    									*/
     									boost = true;
     									break;
     								default:
@@ -134,24 +123,21 @@ public class AgileExtreme {
     							System.out.println("\nThat NPC does not exist");
     						}
     					}
-    					//game.boost();
     					break;
     				case "buget":	
     					System.out.println("\nYour current buget is a small loan of a million");
-    					//game.displayBuget();
+    					game.displayBudget();
     					break;
     				case "progress":
     					System.out.println("\nThe current progress is zero!");
-    					//game.displayProgress();
+    					game.displayProgress();
     					break;
     				case "next":	
-    					if( (days % 7 == 3 ) && boost) {
+    					if( (game.getTime() % 7 == 3 ) && boost) {
     						System.out.println("\nYou can get another boost this week!");
     						boost = false;
     					}
-    					System.out.println("\nBoost is available again!");
-    					//game.update();
-    					days++;
+    					game.update();
     					break;
     				case"skip":
     					if(boost) {
@@ -159,31 +145,24 @@ public class AgileExtreme {
     						boost = false;
     					}
     					System.out.println("\nProceeding to the next day!");
-    					/*
     					for(int x = 0; x < 7; x ++){
     						game.update();
     					}
-    					*/
-    					days += 7;
     					break;
     				case "help":
     					displayOptions();
     					break;
     				case "time":
-    					System.out.println("\nThe current day is " + days + "." );
+    					game.displayTime();
     					break;
     				default:
     					System.out.println("\nThat command does not exist");
     					break;
-
-    			
     			}
     		}
-    		/*
-    		while(game.running()){
-    			
-    		}
-    		*/
+    		scanner.close();
+    		System.out.println("\nThanks for playing!");
+    		System.exit(0);
     }
     private static void startMessage() {
     		System.out.println("Welcome to Agile Extreme!");
@@ -191,7 +170,6 @@ public class AgileExtreme {
     		System.out.println("You direct your team through a series of commands to finish a project.");
     		System.out.println("Your goal is to finish in the least amount of days possible.");
     		System.out.println("Good luck!\n\n\n\n\n");
-    	
     }
     private static void displayOptions() {
     		System.out.println("\nHere is a list of all the possible commands.");
@@ -205,9 +183,5 @@ public class AgileExtreme {
     		System.out.println("stats: Display the stats of all employees.");
     		System.out.println("boost: Motivate your employees!");
     		System.out.println("time: Displays the current day.");
-
-    		
-    		
     }
-  
 }

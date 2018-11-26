588e81e8937de897576b3981e16fa686c6670abb
renzo114 <44302855+renzo114@users.noreply.github.com>
2018-11-25 19:48:26 -0500

    Update AIEnvironment.java

--- AIEnvironment.java
+++ AIEnvironment.java
@@ -1,57 +1,79 @@
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-public class AIEnvironment {
+import java.util.Scanner;
 
-	private double budget = 0;
-	private int days = 0;
-	private double goal = 0;
-
-	public EnviAi()
+public class AIEnvironment{
+	private double budget;
+	private int days;
+	private double goal;
+	private int currentDays;
+	private AINPC npc;
+	private Environment envi;
+	public AIEnvironment() throws FileNotFoundException
 	{
+		envi = new Environment();
+		npc = new AINPC();
 		this.days = 0;
 		this.budget = 0;
 		this.goal = 0;
+		this.currentDays = 0;
 	}
 
 	public boolean hireEmployee()
 	{
 		boolean hired = false;
+		NPC candidate;
+		char input = 'n';
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Are you sure you want to hire Xyz? (y/n)");
-		char input = keyboard.next().charAt(0);
-		if(input == 'y')
-		{
-			// add method from Enviroment class
-			System.out.println("Xyz was hired");
-			hired = true;
-		}
-		else if(input == 'n')
-			System.out.println("Xyz was not hired");
 		
-		else
-			System.out.println("Wrong input");
+		while(!hired)
+		{
+			candidate = npc.getNPC();
+			System.out.println("Hello boss, there is one candidate for the job:");
+			System.out.println("name:\t"+candidate.getName());
+			System.out.println("salary:\t"+candidate.getSalary());
+			System.out.println("level"+candidate.getLevel());
+			System.out.println("Are you sure you want to hire "+candidate.getName()+"? (y/n) b:back");
+			input = keyboard.next().charAt(0);
+			if(input == 'y')
+			{
+				System.out.println("Congratulations !! "+ candidate.getName()+" was hired.");
+				hired = true;
+			}
+			else if(input == 'n')
+				System.out.println(candidate.getName()+" was not hired.");
+			
+			else
+			{
+				System.out.println("No more candidate to report boss.");
+			}
+		}
+		keyboard.close();
 		return hired;
 	}
 	
 	public boolean fireEmployee()
 	{
 		boolean fired = false;
-
+		System.out.println("Here is our team boss: ");
+		envi.displayTeam();
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Are you sure you want to fire Xyz? (y/n)");
-		char input = keyboard.next().charAt(0);
-		if(input == 'y')
+		System.out.println("Who do you want to fire? b: back");
+		String input = keyboard.nextLine();
+		if(input.equalsIgnoreCase("back"))
+			return false;
+		
+		System.out.println("Are you sure you want to fire "+ input +"? (y/n)");
+		char input2 = keyboard.next().charAt(0);
+		if(input2 == 'y')
 		{
-			// remove method from Enviroment class
-			System.out.println("Xyz was fired");
+			envi.fire(input);
+			System.out.println(input+" was fired.");
 			fired = true;
 		}
-		else if(input == 'n')
-			System.out.println("Xyz was not fired");
+		else 
+			System.out.println(input+" was not fired.");
 		
-		else
-			System.out.println("Wrong input");
 		return fired;
 	}
 	
@@ -61,7 +83,7 @@ public class AIEnvironment {
 		// avg income of developers per year 105k
 		budget = (105000/365) * 9;  // avg divided by days in a year and multiply by max # of devs
 		
-		days = (int)(Math.random()*180)+1;
+		days = (int)(Math.random()*180)+7;
 		days = 7;   // FOR TESTING PURPOSES ONLY
 		// final budget + extra
 		budget = (budget*days) + (50*days);
@@ -87,20 +109,63 @@ public class AIEnvironment {
 			probability = (int) (Math.random()*100)+1;
 			if(probability<=percentage)
 			{
-				// pick npc?
-				System.out.println(message);
- 				System.out.println(result);  // test purpose only
+				int index = (int) (Math.random()+envi.atOffice.size()); 
+				NPC employee = envi.atOffice.get(index);
+				System.out.println(employee+""+message);
+ 				System.out.println(result);  // for testing purpose  only
 				return result;
 			}
 		}
 		
-		return "1****";
+		return "0****";
 	}
-	public void update(int d, double b, double g)
+	public String boost()
 	{
-		this.days -= d;
-		this.budget -= b;
-		this.goal -= g;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Which one would you like to use?? (1/2/3) b: go back");
+		System.out.println("1: Buy food for the whole team ($100).");
+		System.out.println("2: Promote someone in the team ($15k).");
+		System.out.println("3: Buy a new Espresso machine for the office ($500).");
+		char input = keyboard.nextLine().charAt(0);
+		switch(input)
+		{
+			case 1: System.out.println("The team loved the food and their productivity has increased.");
+					updateBudget(100);
+					return "2+***";
+			case 2: System.out.println("Who do you want to promote?");
+					envi.displayTeam();
+					String ename = keyboard.nextLine();
+					if(envi.promote(ename))
+						System.out.println(ename+" was promoted.");
+					else
+						System.out.println(ename+" was not promoted");
+					break;
+			case 3: System.out.println("The team loved the Espresso machine and their productivity has increased.");
+					return "2+***";
+			default:System.out.println("No boost was applied.");
+					break;
+		}
+		return "0****";
+	}
+	
+	public void updateDays(int newDays)
+	{
+		this.days -= newDays;
+	}
+	public void updateProgress(int newGoal)
+	{
+		this.goal -= newGoal;
+	}	public void updateBudget(int newBudget)
+	{
+		this.budget -= newBudget;
+	}	
+	public void updateCurrentDays(int newCDays)
+	{
+		this.currentDays += newCDays;
+	}
+	public void setCurrentDays(int newCDays)
+	{
+		this.currentDays = newCDays;
 	}
 	public void setDays(int newDays)
 	{
@@ -116,7 +181,10 @@ public class AIEnvironment {
 	{
 		this.goal = newGoal;
 	}
-	
+	public int getCurrentDays()
+	{
+		return this.currentDays;
+	}
 	public int getDays()
 	{
 		return this.days;
@@ -130,5 +198,17 @@ public class AIEnvironment {
 	public double getGoal()
 	{
 		return this.goal;
+	}
+	public static void main (String[] args) throws FileNotFoundException
+	{
+		EnviAi obj = new EnviAi();
+		
+		for(int i=1; i<=5; i++)
+		{
+			obj.createEvent();
+		}
+		
+		obj.hireEmployee();
+		obj.fireEmployee();
 	}
 }
